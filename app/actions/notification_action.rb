dbec89f0b181fe345d62d66516be86e23d1355b0
Kelly Becker <kellylsbkr@gmail.com>
2012-12-20 17:25:03 -0800

    couple more fixes

--- app/actions/notification_action.rb
+++ app/actions/notification_action.rb
@@ -6,10 +6,10 @@ class NotificationAction < ApplicationAction
 	periodic_timer :send_latest_notifications, :every => 2
 
 	def send_latest_notifications
-		return render [].to_json if @session.user.nil?
+		return render [].to_json if @session.nil? || @session.user.nil?
 
 		@latest_notification ||= nil
-		new_notifications = Notification.owner(@session.user).since(@latest_notification).limit(10)
+		new_notifications = Notification.owner(@session.user).since(@latest_notification).limit(5)
 		@latest_notification = new_notifications.first unless new_notifications.empty?
 
 		# Lets get our data together
